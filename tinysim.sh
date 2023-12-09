14023ff948c135045ec4b2652d2ad7a5dc85098d
burakyueksel <mail.burakyuksel@gmail.com>
2023-12-09 20:40:25 +0100

    add plotting to the sh file

--- tinysim.sh
+++ tinysim.sh
@@ -14,5 +14,5 @@ make -j8
 # Run the simulation
 ./build/tinysim
 
-# Run the Python script
-#python3 ./analyse/plotSim.py
+# Run the Python script. Log file should be in the same folder level as this .sh file
+python3 analyze/plotLog.py
